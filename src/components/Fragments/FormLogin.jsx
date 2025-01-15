62c2249f4d6931fd52bda68c34054dcbf0909262
marzanoo <tanzanowayaw@gmail.com>
2025-01-15 10:32:54 +0700

    event handler

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -2,11 +2,17 @@ import Button from "../Elements/Button"
 import InputForm from "../Elements/Input"
 
 const FormLogin = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        localStorage.setItem('email', event.target.email.value);
+        localStorage.setItem('password', event.target.password.value);
+        window.location.href = "/products";
+    }
     return (
-        <form action="">
+        <form onSubmit={handleLogin}>
             <InputForm label="Email" type="email" name="email" placeholder="example@gmail.com"/>
             <InputForm label="Password" type="password" name="password" placeholder="******"/>
-            <Button classname="bg-blue-600 w-full">Login</Button>
+            <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
         </form>
     )   
 }
