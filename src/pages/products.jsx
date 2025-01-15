8351d0752a7035a4b2f1da9244e4c70091d9b6fc
marzanoo <tanzanowayaw@gmail.com>
2025-01-15 10:04:51 +0700

    render list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,29 +1,35 @@
 import Button from "../components/Elements/Button"
 import CardProduct from "../components/Fragments/CardProduct"
 
+const products = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        image: "/images/shoes-1.jpg",        
+        price: "Rp 1.000.000",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fuga commodi et deleniti qui, voluptate dicta doloribus, praesentium eaque beatae eligendi aliquid similique expedita ut vero odit quod possimus illum.`
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        image: "/images/shoes-1.jpg",
+        price: "Rp 500.000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    }
+]
+
 const ProductPages = () => {
     return (
-        <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg"/>
-                <CardProduct.Body title="Sepatu Baru">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
-                inventore deleniti quos fuga in numquam ipsum aspernatur, 
-                saepe similique quas odio beatae voluptates autem voluptatibus.
-                Quod corporis quidem sapiente voluptates.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000"/>
-            </CardProduct>            
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg"/>
-                <CardProduct.Body title="Sepatu Baru">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, 
-                inventore deleniti quos fuga in numquam ipsum aspernatur, 
-                saepe similique quas odio beatae voluptates autem voluptatibus.
-                Quod corporis quidem sapiente voluptates.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000"/>
-            </CardProduct>            
+        <div className="flex justify-center py-5">            
+            {products.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image}/>
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price}/>
+                </CardProduct> 
+            ))}                                
         </div>        
     )
 }
