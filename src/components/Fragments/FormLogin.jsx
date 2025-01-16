cacd147bded7f96b271b67c6cb64a916810fb9da
marzanoo <tanzanowayaw@gmail.com>
2025-01-16 09:59:45 +0700

    useRef

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,20 +1,40 @@
-import Button from "../Elements/Button"
-import InputForm from "../Elements/Input"
+import { useEffect, useRef } from "react";
+import Button from "../Elements/Button";
+import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
-    const handleLogin = (event) => {
-        event.preventDefault();
-        localStorage.setItem('email', event.target.email.value);
-        localStorage.setItem('password', event.target.password.value);
-        window.location.href = "/products";
-    }
-    return (
-        <form onSubmit={handleLogin}>
-            <InputForm label="Email" type="email" name="email" placeholder="example@gmail.com"/>
-            <InputForm label="Password" type="password" name="password" placeholder="******"/>
-            <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
-        </form>
-    )   
-}
+  const handleLogin = (event) => {
+    event.preventDefault();
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
+    window.location.href = "/products";
+  };
 
-export default FormLogin
+  const emailRef = useRef(null);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+  return (
+    <form onSubmit={handleLogin}>
+      <InputForm
+        label="Email"
+        type="email"
+        name="email"
+        placeholder="example@gmail.com"
+        ref={emailRef}
+      />
+      <InputForm
+        label="Password"
+        type="password"
+        name="password"
+        placeholder="******"
+      />
+      <Button classname="bg-blue-600 w-full" type="submit">
+        Login
+      </Button>
+    </form>
+  );
+};
+
+export default FormLogin;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 import Counter from "../components/Fragments/Counter";
@@ -64,6 +64,23 @@ const ProductPages = () => {
     }
   };
 
+  // useRef
+  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
+
+  const handleAddToCartRef = (id) => {
+    cartRef.current = [...cartRef.current, { id, qty: 1 }];
+    localStorage.setItem("cart", JSON.stringify(cartRef.current));
+  };
+
+  const totalPriceRef = useRef(null);
+  useEffect(() => {
+    if (cart.length > 0) {
+      totalPriceRef.current.style.display = "table-row";
+    } else {
+      totalPriceRef.current.style.display = "none";
+    }
+  }, [cart]);
+
   return (
     <Fragment>
       <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
@@ -115,7 +132,7 @@ const ProductPages = () => {
                   </tr>
                 );
               })}
-              <tr>
+              <tr ref={totalPriceRef}>
                 <td colSpan={3}>
                   <b>Total Price</b>
                 </td>
