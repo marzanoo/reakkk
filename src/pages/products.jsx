e16e2c163d0896d95579e71ed0a8aa37134a01a7
marzanoo <tanzanowayaw@gmail.com>
2025-01-16 08:53:32 +0700

    useState

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,51 +1,115 @@
-import { Fragment } from "react"
-import Button from "../components/Elements/Button"
-import CardProduct from "../components/Fragments/CardProduct"
+import { Fragment, useState } from "react";
+import Button from "../components/Elements/Button";
+import CardProduct from "../components/Fragments/CardProduct";
 
 const products = [
-    {
-        id: 1,
-        name: "Sepatu Baru",
-        image: "/images/shoes-1.jpg",        
-        price: "Rp 1.000.000",
-        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fuga commodi et deleniti qui, voluptate dicta doloribus, praesentium eaque beatae eligendi aliquid similique expedita ut vero odit quod possimus illum.`
-    },
-    {
-        id: 2,
-        name: "Sepatu Lama",
-        image: "/images/shoes-1.jpg",
-        price: "Rp 500.000",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
-    }
-]
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    image: "/images/shoes-1.jpg",
+    price: 1000000,
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere fuga commodi et deleniti qui, voluptate dicta doloribus, praesentium eaque beatae eligendi aliquid similique expedita ut vero odit quod possimus illum.`,
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    image: "/images/shoes-1.jpg",
+    price: 500000,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    name: "Sepatu Adadong",
+    image: "/images/shoes-1.jpg",
+    price: 2000000,
+    description: "Ini sepatu super asli yang make bisa terbang.",
+  },
+];
 
-const email = localStorage.getItem('email');
+const email = localStorage.getItem("email");
 
 const ProductPages = () => {
-    const handleLogout = () => {
-        localStorage.removeItem('email');
-        localStorage.removeItem('password');
-        window.location.href = '/login';
-    }
-    return (
-        <Fragment>
-            <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-                {email}
-                <Button classname="ml-5 bg-black" onClick={handleLogout}>Logout</Button>
-            </div>
-            <div className="flex justify-center py-5">            
-                {products.map((product) => (
-                    <CardProduct key={product.id}>
-                        <CardProduct.Header image={product.image}/>
-                        <CardProduct.Body name={product.name}>
-                            {product.description}
-                        </CardProduct.Body>
-                        <CardProduct.Footer price={product.price}/>
-                    </CardProduct> 
-                ))}                                
-            </div> 
-        </Fragment>       
-    )
-}
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
 
-export default ProductPages
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+        {email}
+        <Button classname="ml-5 bg-black" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        <div className="w-4/6 flex flex-wrap">
+          {products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image} />
+              <CardProduct.Body name={product.name}>
+                {product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer
+                price={product.price}
+                id={product.id}
+                handleAddToCart={handleAddToCart}
+              />
+            </CardProduct>
+          ))}
+        </div>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Qty</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find(
+                  (product) => product.id === item.id
+                );
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>Rp {product.price.toLocaleString("id-ID")}</td>
+                    <td>{item.qty}</td>
+                    <td>
+                      Rp {(product.price * item.qty).toLocaleString("id-ID")}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ProductPages;
