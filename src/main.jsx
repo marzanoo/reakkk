7f9e815559969a68859897f9882074a8da288ccc
marzanoo <tanzanowayaw@gmail.com>
2025-01-16 20:29:09 +0700

    custom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,39 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import ErrorPage from './pages/404';
-import ProductPages from './pages/products';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import ErrorPage from "./pages/404";
+import ProductPages from "./pages/products";
+import ProfilePage from "./pages/profile";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World</div>,
-    errorElement: <ErrorPage/>
+    errorElement: <ErrorPage />,
   },
   {
     path: "/login",
-    element: <LoginPage/>
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <RegisterPage/>
+    element: <RegisterPage />,
   },
   {
     path: "/products",
-    element: <ProductPages/>
-  }
-])
+    element: <ProductPages />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
