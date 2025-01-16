7ccf4bfd9fa61c096902f254a67110588e7ab85a
marzanoo <tanzanowayaw@gmail.com>
2025-01-16 21:52:54 +0700

    dynamic routing

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -81,7 +81,7 @@ const ProductPages = () => {
           {products.length > 0 &&
             products.map((product) => (
               <CardProduct key={product.id}>
-                <CardProduct.Header image={product.image} />
+                <CardProduct.Header image={product.image} id={product.id} />
                 <CardProduct.Body name={product.title}>
                   {product.description}
                 </CardProduct.Body>
